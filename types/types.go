0ba0194303665847de9b231f3e01a0e3491bba9d
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-19 23:35:05 +0530

    helloworld, int, bool, str

---
+++ types/types.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// bool
+	b := true
+	fmt.Println(b)
+
+	var a bool = true
+	fmt.Println(a)
+
+	// int
+	var c int = 5
+	d := 5
+	fmt.Println(c + d)
+	// convert to string
+	str := fmt.Sprintf("%d", d)
+	fmt.Println(str + " is good")
+}
