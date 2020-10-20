613f292cb46056f3582277ee3ba126cfb9e6cfb6
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-20 15:23:16 +0530

    condition

---
+++ condition/condition.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 40
+
+	if age >= 18 { // Cool isnt it
+		fmt.Println("Eligible To Vote")
+	} else {
+		fmt.Println("Not elibible to vote")
+	}
+}
