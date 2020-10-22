7d505ba4a6774fe216458a5d0d8f43b9839e7337
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-22 21:20:56 +0530

    first http server

--- http/handler.go
+++ http/handler.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Helloworld, %s", request.URL.Path)
+}
+
 func main() {
-	http.Ha
+	http.HandleFunc("/", handler)
+	fmt.Println("Running on localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
